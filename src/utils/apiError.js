025b0ddf8764c8ec3e647508058ac7417cf3d9b6
Debanjan110d <debanjangamedu@gmail.com>
2025-08-16 00:24:15 +0530

    feat: add asyncHandler utility for streamlined error handling and ApiError class for standardized error responses

---
+++ src/utils/apiError.js
@@ -0,0 +1,26 @@
+class ApiError extends Error {// Error Is a Build in Js Class
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        // Call parent constructor to initialize Error properties
+        // This is necessary because we are extending the Error class 
+        //Since we are using extends Error Class we have to use super because it is a constructor
+        super();
+        this.statusCode = statusCode;
+        this.data = null
+        this.message = message;
+        this.sucess = false
+        this.errors = errors
+
+
+        if (stack) {
+            this.stack = stack
+
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
---
+++ src/utils/apiResponse.js
@@ -0,0 +1,14 @@
+class ApiResponse {
+    constructor(statusCode, message = "Sucess", data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data
+
+        this.sucess = statusCode < 400
+
+
+    }
+}
+
+
+export { ApiResponse }
