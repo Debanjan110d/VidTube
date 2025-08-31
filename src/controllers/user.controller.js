750b139b243467d41076c8b09b556872319c9f6f
Debanjan110d <debanjangamedu@gmail.com>
2025-08-31 23:14:09 +0530

    feat: implement user login functionality with token generation and error handling; enhance response structure with cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,24 @@ import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.models.js";
 import { upload_cloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js"
+import { connection } from "mongoose";
+
+
+const generateAccessToken_And_RefreshToken = async (userId) => {
+    const user = await User.findById(userId).exec();
+    try {
+        if (!user) {
+            throw new ApiError(404, "User not found");
+        }
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(500, "something went wrong when generating tokens");
+    }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
     //TODO
@@ -62,7 +80,59 @@ const registerUser = asyncHandler(async (req, res) => {
 
 })
 
+const loginUser = asyncHandler(async (req, res) => {
+    //TODO
+    const { username, email, password } = req.body
+    //Validation
+    if (!username?.trim() && !email?.trim()) {
+        throw new ApiError(400, "Either email or username is required")
+    }
+
+    const user = await User.findOne({
+        $or: [
+            { username: username?.toLowerCase() },
+            { email: email?.toLowerCase() }
+        ]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    //validation of password
+    const isValidPassword = await user.isValidPassword(password)
+    if (!isValidPassword) {
+        throw new ApiError(401, "Invalid password")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessToken_And_RefreshToken(user._id)
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+    if (!loggedInUser) {
+        throw new Error("Something went wrong, Logged in user not found in database", "500");
+    }
+    res.status(200).json(new ApiResponse(200, "Login successful", { accessToken, refreshToken }))
+
+    const options = {
+        httpOnly: true,
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        secure: process.env.NODE_ENV === "production",// If this is production then secure should be true other than that false
+        sameSite: "none"
+    }
+    res.cookie("refreshToken", refreshToken, options)
+
+
+
+    return res
+        .status(200)
+        .cookie("refreshToken", refreshToken, options)
+        .cookie("accessToken", accessToken, options)
+        .json(new ApiResponse(200, "Login successful", { loggedInUser, accessToken, refreshToken }))
+}
+)
+
+
 
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
